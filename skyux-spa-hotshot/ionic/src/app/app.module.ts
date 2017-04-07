2c2451c52d6ef6bc31877c9b009e900e95bdd8d1
Isaac Aggrey <github@mail.isaacaggrey.com>
2017-04-06 19:49:08 -0500

    Semi-working ionic social component copy

--- skyux-spa-hotshot/ionic/src/app/app.module.ts
+++ skyux-spa-hotshot/ionic/src/app/app.module.ts
@@ -3,33 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { SocialComponent } from '../pages/social/social.component';
+import { AppNavComponent } from '../pages/shared/app-nav.component';
+import { SkyModule } from '@blackbaud/skyux/dist/core';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    TabsPage,
+    AppNavComponent,
+    SocialComponent,
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    SkyModule,
+    IonicModule.forRoot(MyApp),
+    RouterModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    TabsPage,
+    SocialComponent,
+    AppNavComponent
   ],
   providers: [
     StatusBar,
---
+++ skyux-spa-hotshot/ionic/src/pages/shared/query.service.ts
@@ -0,0 +1,71 @@
+import { Injectable, Output, EventEmitter } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
+
+export interface SearchHit {
+  id? : any;
+  fullName?: string;
+  fields: any;
+}
+
+export interface ErMahBox {
+
+}
+
+export interface Filter {
+
+}
+
+export interface Location {
+  latitude: number;
+  longitude: number;
+}
+
+export interface SearchResult {
+  totalHits?: number;
+  scrollId?: string;
+  searchString?: string;
+  searchHits?: SearchHit[];
+  aggregations?: any;
+}
+
+
+@Injectable()
+export class QueryService {
+  private static _resultsUpdated: EventEmitter<SearchResult>;
+  public location: Location;
+  public distance: number;
+  public socialTags: string[];
+
+
+  constructor(private http: Http) {
+  }
+
+  @Output()
+  public get resultsUpdated(): EventEmitter<SearchResult> {
+    return QueryService._resultsUpdated;
+  }
+
+  public query(): Observable<ErMahBox> {
+    let filters = [];
+    if (this.socialTags.length > 0) {
+      filters.push({"type":"value","field":"social.aiTags",
+        "values":this.socialTags});
+    }
+
+    if (this.location) {
+      filters.push({"type":"geo",
+        "field":"geolocation","distance":`${this.distance}mi`,"latitude":this.location.latitude,"longitude":this.location.longitude,
+        "address":"deez nuts"})
+    }
+    return this.http
+      .post(`https://localhost:8000/api/segment/constituents?set_from=0&set_size=20`,
+          {"filters":filters,
+            "fieldSorts":[{"field":"cons_name.last","ordering":"ASCENDING"},
+              {"field":"cons_name.first","ordering":"ASCENDING"}],"returnedFields":[]}) // NO THIS IS WRONG
+        .map(response => response.json());
+        // .do((results) => QueryService._resultsUpdated.emit(results))
+  }
+}
